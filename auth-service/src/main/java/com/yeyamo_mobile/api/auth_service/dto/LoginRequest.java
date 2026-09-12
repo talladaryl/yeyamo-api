9c734a81c6a2ff42ecbd4edd213e428f3b3fb162
Talla Daryl <talladarylarsen85@gmail.com>
2026-09-12 21:59:41 +0100

    disable turnstile

--- auth-service/src/main/java/com/yeyamo_mobile/api/auth_service/dto/LoginRequest.java
+++ auth-service/src/main/java/com/yeyamo_mobile/api/auth_service/dto/LoginRequest.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.Size;
 public record LoginRequest(
         @NotBlank @Size(max = 254) String identifier,
         @NotBlank @Size(min = 8, max = 128) String password,
-        @NotBlank @Size(max = 4096) String turnstileToken
+        @Size(max = 4096) String turnstileToken
 ) {
 }
